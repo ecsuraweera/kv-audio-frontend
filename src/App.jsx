687a11b37543dce4aea5eae323b1c425d30cc6fd
ecsuraweera <ecsuraweera@gmail.com>
2025-01-20 03:04:22 +0530

    product card

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ProductCard from './components/productCards'
+
+
+function App() {
+ 
+  return (
+    <div>
+      <ProductCard name="Audio Setup" price="3250/-" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, architecto?" img="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp"/>
+     
+     <ProductCard name="Sound Mixer" price="9280/-" description="OYPFXMI Sound Card Microphone Sound Mixer Sound Card Audio Mixing Console Amplifier Live Music Mixer Amplifier DJ Equipment" img="https://img.drz.lazcdn.com/static/lk/p/5306b0159daa15e7b0811a3b71c62cea.png_720x720q80.png_.webp"/>
+    </div>
+  )
+}
+
+export default App
