80713732182d03a93ee94e952a212f56935df48a
ecsuraweera <ecsuraweera@gmail.com>
2025-01-25 01:06:41 +0530

    class react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import './App.css'
-//import ProductCard from './components/productCards'
+import { TbBrandBooking } from 'react-icons/tb';
+import './App.css';
+import ProductCard from './components/productCards';
+import { GoGraph } from "react-icons/go";
+import { MdOutlineSpeaker, MdSupervisedUserCircle } from 'react-icons/md';
+import AdminPage from './components/adminPage';
 
 
 function App() {
     return (
-    <div class="w-full h-screen bg-slate-400">
-    <div className='w-[700px] h-[700px] bg-yellow-700'>
-        <div className= 'w-[500px] h-[500px] bg-black flex flex-row justify-center items-center' >
-            <div className='w-[50px] h-[50px] bg-white'>
 
-                
-
-                </div>
-                <div className='w-[50px] h-[50px] bg-red-700'>
-
-                    </div>
-                    <div className='w-[50px] h-[50px] bg-green-700'>
-
-                    </div>
-        </div> 
-    </div>
-    </div>
-    )
+        <div className="App">
+            <AdminPage/>
+        </div>
+    
+    );
 
   
 }
